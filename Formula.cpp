da7bb53295eed2ee0b049b5dcf0ef7a1ce08905e
leafduo <leafduo@gmail.com>
2009-11-02 18:06:09 +0800

    first commit

---
+++ Formula.cpp
@@ -0,0 +1,33 @@
+inline bool Formula::empty() const
+{
+    return formula.empty();
+}
+
+inline bool Formula::isRPN() const
+{
+    return !RPN.empty();
+}
+
+static inline bool Forluma::assignment()
+{
+    if (var <= maxVar) {
+        ++var;
+        return true;
+    } else
+        return false;
+}
+
+istream& operator>>(istream& in, Formula& f)
+{
+    string str;
+    in >> str;
+    f.formula = str;
+    f.convertRPN();
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Formula& f)
+{
+    out << f.formula;
+    return out;
+}
---
+++ Formula.h
@@ -0,0 +1,31 @@
+#ifndef FORMULA_H
+#define FORMULA_H
+
+#include <string>
+#include <stack>
+#include <istream>
+#include <ostream>
+
+class Formula
+{
+    private:
+        inline bool isRPN() const;
+        inline bool empty() const;
+        bool convertRPN();
+        static inline bool assignment();
+        static unsigned var = 0U;
+        static const unsigned maxVar = pow(2, 26) - 1;
+        bool evaluation() const;
+        string formula;
+        stack<char> RPN;
+    public:
+        Formula();
+        ~Formula();
+        Formula(string str);
+        bool operator==(Formula f) const;
+        Formula& operator=(Formula f);
+        friend istream& operator>>(istream& in, Formula& f);
+        friend ostream& operator<<(ostream& out, const Formula& f);
+};
+
+#endif
