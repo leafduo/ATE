24a6c90b4bc723a43a44f845875ea527fc3c49fd
leafduo <leafduo@gmail.com>
2009-12-25 22:25:27 +0800

    welcome

--- ATE.cpp
+++ ATE.cpp
@@ -8,18 +8,19 @@ int main(int argc, const char* const argv[])
 {
     Formula f1, f2;
     if (1 == argc) {
-    cout << "prompt:";
-    cout << "f1:";
+    cout << "Welcome \"Are they equivalent?\", would you want to know if they are equivalent?" << endl
+        <<"Then please input two formulae. :)" << endl;
+    cout << "Formula 1:";
     cin >> f1;
-    cout << "f2:";
+    cout << "Formula 2:";
     cin >> f2;
     } else {
         f1 = argv[1];
         f2 = argv[2];
     }
     if (f1 == f2)
-        cout << "equivalent" << endl;
+        cout << "Yes, they are equivalent." << endl;
     else
-        cout << "not equivalent" << endl;
+        cout << "Sorry, they are not equivalent. :(" << endl;
     return 0;
 }
